edeb75eed7248357fe0898f99f8eea26af4a390d
Trinsore <oreofeoluwataiwo93@gmail.com>
2023-03-23 04:20:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  * _isupper - uppercase letters
  * @c: char to check
  * Return: 0 or 1
  */
+
 int _isupper(int c)
+
 {
-	if (c >= 'A' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	if (c >= '65' && c <= '98')
+	{
+	return (1);
+	}
+	return (0);
 }
