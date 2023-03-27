032f49ae6e09d04b0086cc163cb87451cf78592a
Trinsore <oreofeoluwataiwo93@gmail.com>
2023-03-27 03:27:46 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,15 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: void
  */
+
 void _puts(char *str)
+
 {
-	while (*str != '\0')
+	for (*str != '\0'; str++)
 	{
 		_putchar(*str++);
 	}
-		_putchar|('\n');
+		_putchar('\n');
 }
