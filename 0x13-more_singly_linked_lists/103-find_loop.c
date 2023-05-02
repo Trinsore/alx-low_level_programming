0553b3ecddfc1317c1e64ff361cee13a44ef5c9c
Trinsore <oreofeoluwataiwo93@gmail.com>
2023-05-02 07:27:17 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop contained in 
+ * find_listint_loop - Finds the loop contained in
  * a listint_t linked list
  * @head: A pointer to the head of the listint_t list.
  *
